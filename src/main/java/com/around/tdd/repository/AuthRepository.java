756e72ee9e232448d933ddf30dc1737f2e701cec
HwangJunha <tarot1415@danawa.com>
2024-08-13 10:53:25 +0900

    [master] 사용하지 않은 파일 삭제

--- src/main/java/com/around/tdd/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-package com.around.tdd.repository;
-
-import com.around.tdd.vo.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface AuthRepository extends JpaRepository<Member, Long> {
-}
