e43405ee3d7a027182d1fb2ad9182e4c440aa244
Hanseul <ihanseul731@danawa.com>
2024-09-02 11:11:35 +0900

    [4-feature] 장바구니 VO 구성

--- src/main/java/com/around/tdd/vo/Cart.java
+++ src/main/java/com/around/tdd/vo/Cart.java
@@ -1,22 +1,27 @@
 package com.around.tdd.vo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@RequiredArgsConstructor
+@Setter
 @Getter
 public class Cart {
     @Id
-    @GeneratedValue
+    @Column(name = "cart_seq", nullable = false)
     private Long cartSeq;
 
+    @Column(name ="member_seq", nullable = false)
     private Long memberSeq;
 
+    @Column(name = "product_seq", nullable = false)
     private Long productSeq;
 
+    @Column(name="product_num", nullable = false)
     private Integer productNum;
 }
