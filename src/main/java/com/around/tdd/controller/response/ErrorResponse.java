2e3ec4f29f9dabcd3d8fbdd95149f2a1632c9e1b
suea724 <suea724@gmail.com>
2024-09-05 10:13:55 +0900

    [9-refactor] ApiResponse, ErrorResponse 코드 리뷰 반영

--- src/main/java/com/around/tdd/controller/response/ErrorResponse.java
+++ src/main/java/com/around/tdd/controller/response/ErrorResponse.java
@@ -1,25 +1,24 @@
 package com.around.tdd.controller.response;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Map;
 
 @Getter
-@Setter
-public class ErrorResponse {
-    private int status;
-    private String message;
-    private LocalDateTime timestamp;
-    private String requestPath;
-    private Map<String, String> errors;
+public final class ErrorResponse {
+    private final int status;
+    private final String message;
+    private final LocalDateTime timestamp;
+    private final String requestPath;
+    private final Map<String, String> errors;
 
     public ErrorResponse(int status, String message, LocalDateTime timestamp, String requestPath, Map<String, String> errors) {
         this.status = status;
         this.message = message;
         this.timestamp = timestamp;
         this.requestPath = requestPath;
-        this.errors = errors;
+        this.errors = (errors != null) ? Collections.unmodifiableMap(errors) : Collections.emptyMap();
     }
 }
